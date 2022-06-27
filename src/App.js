935b0af026e5d386863019825fc09064d62a2c2d
Jordan Trempert <32543343+jordan-trempert@users.noreply.github.com>
2022-06-27 16:12:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,18 +54,36 @@ function Home() {
               />
             </center>
             <br />
-            <center> What's new in Beta 4: </center>
+            <center> What's new in Beta 5: </center>
             <br />
-            - Vulcan
+            - Space
             <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;- A very hot and volcanic world
+            &nbsp;&nbsp;&nbsp;&nbsp;- 6 Story Batteries
             <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;- 1 Story Batteries
+            &nbsp;&nbsp;&nbsp;&nbsp;- 6 Planets
             <br />
             &nbsp;&nbsp;&nbsp;&nbsp;- 1 Boss fight
             <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;- Large story implications
-            <br />- Various bug fixes
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <center>Various bug fixes </center>
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;- Fixed an issue where you weren't able to move in the Bruce bossfight
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;- Not dying after losing all of your health in the Hospital
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;- Helicopter flys through bottom and top walls
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;- Health upon entering a world isn't full
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;- Undertale heart clipping
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;- Arcadia boss no movement animation for the final 2 attack sequences
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;- Many bugs making the game impossible to progress on Android
+
+    
+            
           </div>
         </span>
         <br />
